4b7ae8a3aae39d406a0a3baa07f34a0318bed04a
Peter Matthies <petruxa0708@gmail.com>
2018-02-01 12:58:10 +0100

    new scripts for plotting data for sample ch2700(1)

--- plot_freq_sweep_ch2700.py
+++ plot_freq_sweep_ch2700.py
@@ -3,9 +3,15 @@ import matplotlib.pyplot as plt
 
 
 # path_to_data = './m_data/30052017_nrcl/'
+# path_to_data = './m_data/30012018_ncrl/m6/'
 path_to_data = './m_data/30012018_ncrl/'
-data_file_1 = 'm3_pos2.txt'
-data_file_2 = 'm3_pos1.txt'
+# data_file_1 = 'm6_f06_pos2.txt'
+# data_file_2 = 'm6_f06_pos1.txt'
+# field_values_file ='m6_field_values.txt'
+
+data_file_1 = 'm8_pos2.txt'
+data_file_2 = 'm8_pos1.txt'
+
 
 fig = plt.figure()
 
@@ -14,9 +20,12 @@ ax1.set_xlabel('Frequency (GHz)', fontsize=15)
 ax1.set_ylabel('Intensity (a.u.)', fontsize=15)
 ax1.tick_params(axis='both', which='major', labelsize=12)
 
+#field_data = np.loadtxt(path_to_data+field_values_file)
+#magn_field = field_data[:, 1][int(data_file_1[4:6])-1] * 1000
 
-magn_field = '103mT'
-fig.suptitle('Freq Spectra for '+magn_field)
+magn_field = -0.73
+
+fig.suptitle('Freq Spectra for '+str(magn_field)+ ' mT')
 
 data_1 = np.loadtxt(path_to_data + data_file_1, comments='#')
 x_pos1 = data_1[:, 0] / 1e9
@@ -29,7 +38,7 @@ y_pos2 = data_2[:, 1] # /max(data[:, 1])
 ax1.plot(x_pos1, y_pos1, color='blue', label='above antenna (+k)')
 ax1.plot(x_pos2, y_pos2, color='red', label='below antenna (-k)')
 
-ax1.set_xlim([10.0, 20.0])
+ax1.set_xlim([min(x_pos1), max(x_pos1)])
 max_y = max(np.maximum(y_pos1, y_pos2))
 print(max_y)
 ax1.set_ylim([0, max_y+0.05*max_y])
@@ -39,6 +48,7 @@ fig.tight_layout()
 fig.subplots_adjust(top=0.95)
 plt.grid()
 # plt.legend(loc=1, bbox_to_anchor=(1.05, 1.0), prop={'size': 14})
-plt.legend(loc=1, prop={'size': 14})
-plt.savefig('./output_pics/nrcl/'+'freq_spec_h3_'+magn_field+'.png', format='png', dpi=100)
+plt.legend(loc=2, prop={'size': 14})
+magn_field_save = str(magn_field).replace('.', 'p') + 'mT'
+plt.savefig('./output_pics/nrcl/h3/'+'freq_spec_h3_'+magn_field_save+'.png', format='png', dpi=100)
 plt.show()
